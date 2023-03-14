485f080197063d2a5da5d13a62158b72ad586f3b
marek_gerszendorf <marek.gerszendorf@wp.pl>
2023-03-14 18:19:46 +0100

    Initial Commit

---
+++ UberSwiftUITutorial/UberSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUITutorialApp.swift
+//  UberSwiftUITutorial
+//
+//  Created by Marek Gerszendorf on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
